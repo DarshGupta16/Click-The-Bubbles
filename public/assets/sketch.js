db60ff8c460b2126592af3878a7d5c6377a9be60
DarshGupta16 <somefakeemail@gmail.com>
2020-11-09 20:22:07 +0530

    Adding the bubble class

---
+++ public/assets/sketch.js
@@ -0,0 +1,11 @@
+class Bubble {
+  constructor(x, y, diameter) {
+    this.x = x;
+    this.y = y;
+    this.diameter = diameter;
+    this.radius = this.diameter / 2;
+    this.r = 0;
+    this.g = 0;
+    this.b = 0;
+  }
+}
